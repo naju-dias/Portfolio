05aace8c5cf0c26e884091dc3da9f016d1b42a12
naju-dias <anajuliaalvesd10@gmail.com>
2026-08-09 16:20:33 -0300

    otimização

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,20 @@
 
 import dynamic from 'next/dynamic';
 import Hero from "@/components/sections/Hero";
-import Projects from "@/components/sections/Projects";
-import About from "@/components/sections/About";
-import Skills from "@/components/sections/Skills";
-import Contact from "@/components/sections/Contact";
-import Footer from "@/components/sections/Footer";
 
 import { useBgTransition } from "@/hooks/useBgTransition";
 import { useIsMobile } from "@/hooks/useIsMobile";
 import { useIdleMount } from "@/hooks/useIdleMount";
 
+const Projects = dynamic(() => import("@/components/sections/Projects"));
+const About = dynamic(() => import("@/components/sections/About"));
+const Skills = dynamic(() => import("@/components/sections/Skills"));
+const Contact = dynamic(() => import("@/components/sections/Contact"));
+const Footer = dynamic(() => import("@/components/sections/Footer"));
+
+// Não baixar Three.js no Mobile!
 const Lanyard = dynamic(
-  () => import(/* webpackPrefetch: true */ "@/components/effects/Lanyard"),
+  () => import("@/components/effects/Lanyard"),
   { ssr: false }
 );
 
@@ -31,8 +33,12 @@ export default function Home() {
 
   return (
     <main className="min-h-screen">
-      {isMobile === true && <LoadingScreenMobile />}
-      {isMobile === false && <LoadingScreen />}
+      <div className="block xl:hidden">
+        <LoadingScreenMobile />
+      </div>
+      <div className="hidden xl:block">
+        <LoadingScreen />
+      </div>
 
       <div className="relative w-full">
         <Hero>
