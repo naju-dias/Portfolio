d4a313e5144e0cae4389736438ce03d4455058ed
naju-dias <anajuliaalvesd10@gmail.com>
2026-08-09 15:10:24 -0300

    .

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,15 @@
 import type { NextConfig } from "next";
 import bundleAnalyzer from "@next/bundle-analyzer";
 
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
 const nextConfig: NextConfig = {
   reactStrictMode: false,
   images: {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384, 480, 640, 768, 828],
   },
 };
-
-module.exports = nextConfig;
-
-const withBundleAnalyzer = bundleAnalyzer({
-  enabled: process.env.ANALYZE === "true",
-});
 
 export default withBundleAnalyzer(nextConfig);
